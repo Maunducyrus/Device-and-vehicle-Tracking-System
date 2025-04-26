89bb4f89972a6d9da950c7a0674f88b40de1fb54
Maunducyrus <maunducyrus123@gmail.com>
2025-04-26 22:24:13 +0300

    Models in Alert

--- Backend/alerts/models.py
+++ Backend/alerts/models.py
@@ -1,1 +1,12 @@
 # Create your models here.
+
+from django.db import models
+
+class Alert(models.Model):
+    ALERT_TYPES = [
+        ('theft', 'Theft'),
+        ('unauthorized', 'Unauthorized Access'),
+        ('suspicious', 'Suspicious Activity'),
+        ('violence', 'Violence'),
+        ('other', 'Other'),
+    ]
