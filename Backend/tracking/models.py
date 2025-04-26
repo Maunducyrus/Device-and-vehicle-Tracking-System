79e482e69973f002fbb6b24970600d896c7934b1
Maunducyrus <maunducyrus123@gmail.com>
2025-04-26 22:01:24 +0300

    tacking models

--- Backend/tracking/models.py
+++ Backend/tracking/models.py
@@ -9,3 +9,14 @@ class Device(models.Model):
         ('laptop', 'Laptop'),
         ('other', 'Other'),
     ]
+
+    owner_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    serial_number = models.CharField(max_length=100, blank=True, null=True)
+    imei = models.CharField(max_length=100, blank=True, null=True)
+    mac_address = models.CharField(max_length=100, blank=True, null=True)
+    device_type = models.CharField(max_length=20, choices=DEVICE_TYPE_CHOICES)
+    last_latitude = models.FloatField(blank=True, null=True)
+    last_longitude = models.FloatField(blank=True, null=True)
+    is_stolen = models.BooleanField(default=False)
+    update_at = models.DateTimeField(auto_now=True)
