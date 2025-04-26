bcbf50e52cb6421e2d2abb11fd966aa80897b142
Maunducyrus <maunducyrus123@gmail.com>
2025-04-26 22:12:38 +0300

    Class Vehicle in models

--- Backend/tracking/models.py
+++ Backend/tracking/models.py
@@ -23,3 +23,19 @@ class Device(models.Model):
 
     def __str__(self):
         return f"{self.device_type} - {self.owner_name}"
+    
+class Vehicle(models.Model):
+    owner_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    license_plate = models.CharField(max_length=100, unique=True)
+    make_model = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    year = models.IntegerField(max_length=10)
+    rfid_tag = models.CharField(max_length=100, blank=True, null=True)
+    last_latitude = models.FloatField(blank=True, null=True)
+    last_longitude = models.FloatField(blank=True, null=True)
+    is_stolen = models.BooleanField(default=False)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.license_plate} - {self.owner_name}"
