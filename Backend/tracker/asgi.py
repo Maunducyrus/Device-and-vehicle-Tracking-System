57126cb4cf2184cc4ad78bef347dd2c7027daa02
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 22:51:55 +0300

    asqi.py websockets

--- Backend/tracker/asgi.py
+++ Backend/tracker/asgi.py
@@ -14,3 +14,7 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracker.settings')
 
 application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
