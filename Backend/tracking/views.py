fe05336d78969a3643ed1273c0789b04a651967d
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 21:10:56 +0300

    search device function

--- Backend/tracking/views.py
+++ Backend/tracking/views.py
@@ -15,3 +15,12 @@ class DeviceCreateView(generics.CreateAPIView):
 class VehicleCreateView(generics.CreateAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+
+# Device Search
+@api_view(['GET'])
+def device_search(request):
+    search_type = request.query_params.get('search_type')
+    search_value = request.query_params.get('search_value')
+    devices = Device.objects.filter(**{search_type: search_value})
+    serializer = DeviceSerializer(devices, many=True)
+    return Response(serializer.data)
