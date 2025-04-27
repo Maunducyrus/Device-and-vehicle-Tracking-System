690ff617eafeed14c4df03cc42495e8a5d9c49b5
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 21:32:38 +0300

    views in alerts

--- Backend/alerts/views.py
+++ Backend/alerts/views.py
@@ -4,3 +4,13 @@
 from rest_framework import generics
 from .models import Alert
 from .serializers import AlertSerializer
+
+# Alert registration
+class AlertCreateView(generics.CreateAPIView):
+    queryset = Alert.objects.all()
+    serializer_class = AlertSerializer
+
+# Alert listing
+class AlertListView(generics.ListAPIView):
+    queryset = Alert.objects.all()
+    serializer_class = AlertSerializer
