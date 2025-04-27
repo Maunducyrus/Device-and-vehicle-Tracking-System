2b70cd3ad0620bf783b0eb246bc76ed54e160584
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 21:07:11 +0300

    creating device

--- Backend/tracking/views.py
+++ Backend/tracking/views.py
@@ -5,3 +5,8 @@ from rest_framework.response import Response
 from .models import Device, Vehicle
 from .serializers import DeviceSerializer, VehicleSerializer
 from rest_framework.decorators import api_view
+
+# Device Registration
+class DeviceCreateView(generics.CreateAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
