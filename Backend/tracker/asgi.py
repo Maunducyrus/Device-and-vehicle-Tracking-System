bc3308ab403a6c8404d1153ec8faa632e59e1763
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 23:46:58 +0300

    asgi.py in tracker

--- Backend/tracker/asgi.py
+++ Backend/tracker/asgi.py
@@ -10,11 +10,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracker.settings')
+# application = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from tracking.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracker.settings')
 
-application = get_asgi_application()
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            tracking.routing.websocket_urlpatterns
+        )
+    ),
+})
