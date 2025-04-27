34928b0f88a4f0d4ebaaa83c741ff34ad8848623
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 22:02:40 +0300

    urls in alerts

--- Backend/alerts/urls.py
+++ Backend/alerts/urls.py
@@ -1,3 +1,8 @@
 # alerts/urls.py
 from django.urls import path
 from .views import AlertCreateView, AlertListView
+
+urlpatterns = [
+    path('alerts/', AlertCreateView.as_view(), name='alert-create'),
+    path('alerts/list/', AlertListView.as_view(), name='alert-list'),
+]
