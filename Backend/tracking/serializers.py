c2304b460686af4eda0b30748940e0d959f689d4
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 20:11:34 +0300

    serializers

--- Backend/tracking/serializers.py
+++ Backend/tracking/serializers.py
@@ -1,3 +1,13 @@
 # tacking/serializers.py
 from rest_framework import serializers
 from .models import Device, Vehicle
+
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = '__all__'
+
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
