ce3a0838794f785164b7dbf1836bccaff9d3b07b
Maunducyrus <maunducyrus123@gmail.com>
2025-04-27 23:58:25 +0300

    consumers.py

--- Backend/tracking/consumers.py
+++ Backend/tracking/consumers.py
@@ -9,4 +9,18 @@ class LocationConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-            await self.channel_layer.group_discard('Location_updates', self.channel_name)
+        await self.channel_layer.group_discard('Location_updates', self.channel_name)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            'Location_updates',
+            {
+                'type': 'location_update',
+                'data': data
+            }
+        )
+
+    async def send_location(self, event):
+        await self.send(text_data=json.dumps(event['data']))
+         
