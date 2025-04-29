1ebc49eb21cdbe3bfca1f7059ca32c46f7cec890
Maunducyrus <maunducyrus123@gmail.com>
2025-04-29 15:50:56 +0300

    routing

--- Backend/tracking/routing.py
+++ Backend/tracking/routing.py
@@ -1,9 +1,11 @@
 # tracking/routing.py
 
 from django.urls import re_path
-from . import consumers
+# from . import consumers
+from .consumers import LocationConsumer
 
 websocket_urlpatterns = [
     # re_path(r'ws/location/$', consumers.LocationConsumer.as_asgi()),
-        re_path(r'^ws/location/$', consumers.LocationConsumer.as_asgi()),
+    # re_path(r'^ws/location/$', consumers.LocationConsumer.as_asgi()),
+    re_path(r'ws/location/$', LocationConsumer.as_asgi()),
 ]
